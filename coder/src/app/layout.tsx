a52c1edf0fbbe127da3a82cb1978298c07598664
wael <78425538+wa20@users.noreply.github.com>
2024-05-28 14:58:44 +0100

    added theme provided and created initial header

--- coder/src/app/layout.tsx
+++ coder/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem 
+          disableTransitionOnChange
+        >
+        <header className="border-b sticky top-0 bg-white z-50 shadow-md text-neutral-500">
+          <Header/>
+        </header>
+          {children}
+        </ThemeProvider>
+        
+        </body>
     </html>
   );
 }
---
+++ coder/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ModeToggle } from './ModeToggle'
+
+
+function Header() {
+  return (
+    <div className='p-3 '>
+        <ModeToggle />
+    </div>
+  )
+}
+
+export default Header
