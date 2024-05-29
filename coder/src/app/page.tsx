bb67dda7bf9807f9580dfd0b7325dfcf6a19e80a
wael <78425538+wa20@users.noreply.github.com>
2024-05-29 10:01:39 +0100

    minor amends to layout and page files

--- coder/src/app/page.tsx
+++ coder/src/app/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import { db } from "@/db";
 
-export default function Home() {
+export default async function Home() {
+
+  const items = await db.query.testing.findMany();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home page</h1>
+      {/* <h1>
+
+
+        
+
+      </h1> */}
+
+      {items.map((item) => (
+          <div key={item.id}>
+            <h1>{item.name}</h1>
+          </div>
+        ))}
+
     </main>
   );
 }
