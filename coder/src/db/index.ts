28c14b8c37ad2612e1fee50f9e5459b6ee9912bd
wael <78425538+wa20@users.noreply.github.com>
2024-05-29 10:12:26 +0100

    created index and schema files for drizzle orm

---
+++ coder/src/db/index.ts
@@ -0,0 +1,16 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+// import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+import * as schema from './schema';
+
+// for migrations
+// const migrationClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db", { max: 1 });
+// migrate(drizzle(migrationClient), ...)
+
+// for query purposes
+const queryClient = postgres(process.env.DB_URL!);
+const db = drizzle(queryClient, { schema });
+// await db.select().from(...)...
+
+
+export { db };
